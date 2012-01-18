d9bb6aee383815ec18aa8b22e4149ca73f8913e0
Johan Sveholm <jsveholm@gmail.com>
2012-01-18 22:50:04 +0900

    whatever

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120117043724) do
+ActiveRecord::Schema.define(:version => 20120118044053) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
