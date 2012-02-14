26b35487c7ca9ea6499cbcf6359ce46b2923f846
Johan Sveholm <jsveholm@gmail.com>
2012-02-14 18:34:02 +0900

    signup confirmation

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -5,10 +5,10 @@ describe UserMailer do
   include EmailSpec::Matchers
 
   it "sends a signup mail" do
-    user = Factory(:user,:email => 'jsveholm@gmail.com')
-    token = user.signup_tokens.first.token
-    @email = UserMailer.signup(user,signup_confirmation_url(token))
+    user = Factory(:user,email:'jsveholm@gmail.com')
+    token = SignupToken.create(email:user.email)
+    @email = UserMailer.signup(user,signup_confirmation_url(token.token))
     @email.should deliver_to('jsveholm@gmail.com')
-    @email.should have_body_text(/Activation link:.*#{token}/)
+    @email.should have_body_text(/Activation link:.*#{token.token}/)
   end
 end
