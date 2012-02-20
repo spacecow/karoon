da86a20f4f81cb242620202812229a5fb55fbc4e
Johan Sveholm <jsveholm@gmail.com>
2012-02-20 17:50:12 +0900

    whatever

--- spec/requests/categories_edit_spec.rb
+++ spec/requests/categories_edit_spec.rb
@@ -12,47 +12,10 @@ describe "Categories" do
       visit edit_category_path(@science)
     end
 
-    it "layout" do
-      page.should have_title('Edit Category')
-      find_field('Name').value.should eq "science"
-      options('Parent').should eq "BLANK, religion, religion/mantra"
-      page.should have_button('Update Category')
-    end
-
     context "edit category" do
       before(:each) do
         fill_in 'Name', :with => "space" 
         select 'religion/mantra', :from => 'Parent'
-      end
-
-      it "new version is saved to database" do
-        lambda do
-          click_button 'Update Category'
-        end.should change(Category,:count).by(0)
-        Category.find(@science.id).name.should eq 'space'
-        Category.find(@science.id).names_depth_cache_en.should eq 'religion/mantra/space'
-        Category.find(@science.id).parent.should eq @mantra
-      end
-
-      it "descendants names_depth_cache is updated" do
-        click_button 'Update Category'
-        Category.find(@rocket.id).names_depth_cache_en.should eq 'religion/mantra/space/rocket'
-      end
-
-      it "name cannot be blank" do
-        fill_in 'Name', :with => ''
-        click_button 'Update Category'
-        li(:name).should have_blank_error
-      end
-      it "name must be unique" do
-        create_category('space')
-        click_button 'Update Category'
-        li(:name).should have_duplication_error
-      end
-      it "options should remain on error page" do
-        fill_in 'Name', :with => ''
-        click_button 'Update Category'
-        options('Parent').should eq "BLANK, religion, religion/mantra"
       end
 
       it "shows a flash message" do
