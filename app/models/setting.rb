697503a526b2ad11f3c146ddcaeef8ed3e8c3df5
Johan Sveholm <jsveholm@gmail.com>
2012-02-23 19:05:39 +0900

    some rial corrections

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,12 +4,7 @@ class Setting < ActiveRecord::Base
   CURRENCY = [RIAL, TOMAN]
 
   class << self
-    #def currency_in_riel?;
-    #  singleton.currency == RIEL
-    #end
-    #def currency_in_toman?;
-    #  singleton.currency == TOMAN 
-    #end
+    def currencies; CURRENCY.map{|e| I18n.t(e)} end
     def in_currency; I18n.t("in_#{singleton.currency}") end 
     def singleton; Setting.first end
   end
